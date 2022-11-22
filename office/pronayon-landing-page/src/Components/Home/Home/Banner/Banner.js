437d81c10874c056f706e269fec92356af28c348
Shadman Saif Anonno <anonno77@gmail.com>
2022-11-23 01:34:24 +0600

    navbar and banner fix

--- office/pronayon-landing-page/src/Components/Home/Home/Banner/Banner.js
+++ office/pronayon-landing-page/src/Components/Home/Home/Banner/Banner.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import maskgroup1 from '../../../../Images/MaskGroup1.png';
-import Group1 from '../../../../Images/Group1.png'
+import React from "react";
+import maskgroup1 from "../../../../Images/MaskGroup1.png";
+import Group1 from "../../../../Images/Group1.png";
 // import arc3 from '../../../../Images/arc3.jpg'
-import arc4 from '../../../../Images/arc4.jpeg'
-import arc6 from '../../../../Images/arc6.jpg'
-import img5 from '../../../../Images/project/5.jpg'
-import img6 from '../../../../Images/project/12.jpg'
-import img7 from '../../../../Images/project/14.jpg'
-import img8 from '../../../../Images/project/15.jpg'
+import arc4 from "../../../../Images/arc4.jpeg";
+import arc6 from "../../../../Images/arc6.jpg";
+import img5 from "../../../../Images/project/5.jpg";
+import img6 from "../../../../Images/project/12.jpg";
+import img7 from "../../../../Images/project/14.jpg";
+import img8 from "../../../../Images/project/15.jpg";
 // import React, { useState } from 'react';
 //   import { Swiper, SwiperSlide } from 'swiper/react';
 //   import { Thumbs } from 'swiper';
 
 // Import Swiper styles
-import 'swiper/css';
-import './Banner.css';
-
+import "swiper/css";
+import "./Banner.css";
 
 const Banner = () => {
-    // const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  // const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
-    return (
-        <div>
-            {/* <Swiper
+  return (
+    <div className="container">
+      {/* <Swiper
       spaceBetween={50}
       slidesPerView={3}
       onSlideChange={() => console.log('slide change')}
@@ -35,75 +34,127 @@ const Banner = () => {
       ...
     </Swiper> */}
 
-<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 4"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src={ img5} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src={img6} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={img7} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={img8} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 p-4">
-                      <h1 className='banner-h1'>PRONAYON</h1>
-                        <h5 className="banner-txt w-50">PRONAYON, a concern of Capco Group, is one of the most dynamic Architectural, Engineering, and Planning Consultants in Bangladesh with market leading position in Chittagong.</h5>
-                        <img className="p-3" src={maskgroup1}  alt=""/>
-
-                    </div>
-                    <div className="col-md-4 p-4 banner-left">
-                        <h1 className='banner-left-head'>31 </h1>
-                        <h6 className='banner-h6'>Years experience</h6>
-                        <h1 className='banner-left-head'>2000 </h1>
-                        <h6 className='banner-h6'>References of projects</h6>
-                        <h5 className='banner-h5'>A committed and competent group of multi-deciplined professionals.</h5>
-                        <p className='banner-p'>It is a team of committed and versatile professionals, dedicated to idealize new thinking and bewitching designs and architecture. We, the PRONAYON family, is promised to evaluate your imaginary concept and to incarnate your dream into the ultimate completion</p>
-
-                    </div>
-                </div>
-
-            </div>
+      <div
+        id="carouselExampleDark"
+        class="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 4"
+          ></button>
         </div>
-    );
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src={img5} class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src={img6} class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={img7} class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src={img8} class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 p-4">
+            <h1 className="banner-h1">PRONAYON</h1>
+            <h5 className="banner-txt w-50">
+              PRONAYON, a concern of Capco Group, is one of the most dynamic
+              Architectural, Engineering, and Planning Consultants in Bangladesh
+              with market leading position in Chittagong.
+            </h5>
+            <img className="p-3" src={maskgroup1} alt="" />
+          </div>
+          <div className="col-md-4 p-4 banner-left">
+            <h1 className="banner-left-head">31 </h1>
+            <h6 className="banner-h6">Years experience</h6>
+            <h1 className="banner-left-head">2000 </h1>
+            <h6 className="banner-h6">References of projects</h6>
+            <h5 className="banner-h5">
+              A committed and competent group of multi-deciplined professionals.
+            </h5>
+            <p className="banner-p">
+              It is a team of committed and versatile professionals, dedicated
+              to idealize new thinking and bewitching designs and architecture.
+              We, the PRONAYON family, is promised to evaluate your imaginary
+              concept and to incarnate your dream into the ultimate completion
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- office/pronayon-landing-page/src/Components/Home/Shared/Navbar/Navbar.js
+++ office/pronayon-landing-page/src/Components/Home/Shared/Navbar/Navbar.js
@@ -1,50 +1,87 @@
-import React from 'react';
+import React from "react";
 // import {Nav,Link,Container} from 'react-bootstrap';
-import logo from '../../../../Images/logo.png';
+import logo from "../../../../Images/logo.png";
+import "./Navbar.css"
 
 const Navbar = () => {
-    return (
-        <div  className="container" >
-     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light nav ">
-    <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-    {/* <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
-  </form>
-  <div class="container-fluid   ">
-    <a class="navbar-brand justify-content-end" href="#"></a>
-       <img src={logo} alt=""/>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse ustify-content-end" id="navbarNavDropdown">
-      <ul class="navbar-nav  w-50 p-3">
-        <li class="nav-item  ">
-          <a class="nav-link active " aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-        </div>
-    );
+  return (
+    <nav class="navbar-expand-lg navbar navbar-light container custom-nav">
+      <form class="form-inline">
+        <input
+          class="form-control"
+          type="search"
+          placeholder="Search..."
+          aria-label="Search"
+        />
+        {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
+      </form>
+      <a class="navbar-brand" href="#">
+        <img src={logo} alt="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active " aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Features
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+          {/* <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown link
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li> */}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

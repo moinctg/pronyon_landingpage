d5444dafbde87d8ea39b0c30e68008bdf752c7e1
Shadman Saif <anonno77@gmail.com>
2023-01-21 00:25:44 +0600

    services done desktop only

--- office/pronayon-landing-page/src/Components/Home/Service.jsx
+++ office/pronayon-landing-page/src/Components/Home/Service.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
 import styles from "./Service.module.css";
 import SectionHeaderOne from "./Shared/SectionHeaderOne";
+import { getAll } from "../../Utils/api";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Service = () => {
+  const scrollServiceContainer = (direction) => {
+    try {
+      var div = document.getElementById("serviceContainer");
+      const divWidth = div.scrollWidth - div.clientWidth;
+      if (direction === "right")
+        div.scrollLeft = div.scrollLeft + Math.floor(divWidth * 0.4);
+      else div.scrollLeft = div.scrollLeft - Math.floor(divWidth * 0.4);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [services, setServices] = useState({});
+  useEffect(() => {
+    setServices(getAll("services"));
+  });
   return (
     <div
       className={`container ${styles.service_container} pt-5`}
@@ -9,6 +30,7 @@ const Service = () => {
       data-aos-delay="50"
     >
       <SectionHeaderOne title="SERVICES" />
+      {/* Summary & Buttons */}
       <div className="row">
         <div className="col-md-9">
           <p
@@ -27,8 +49,42 @@ const Service = () => {
             interest of the Clients.
           </p>
         </div>
-        <div className="col-md-3">Button goes here</div>
+        <div className="col-md-3 text-center">
+          <div className={[styles.services_btn]}>
+            <button
+              onClick={() => {
+                scrollServiceContainer("left");
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+            <button
+              onClick={() => {
+                scrollServiceContainer("right");
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          </div>
+        </div>
       </div>
+      {/* Summary & Buttons */}
+      {/* Services */}
+      <div id="serviceContainer" className={[styles.service_grp]}>
+        {Object.keys(services).map((department, index) => (
+          <div>
+            <h5 className={[styles.service_heading]} key={index}>
+              {department}
+            </h5>
+            {services[department].map((service, serviceIndex) => (
+              <p key={serviceIndex}>
+                {serviceIndex + 1} {service}
+              </p>
+            ))}
+          </div>
+        ))}
+      </div>
+      {/* Services */}
     </div>
   );
 };
